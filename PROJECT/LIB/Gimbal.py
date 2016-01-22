2b44ee8e65dd95dc20e205b0ed44c4fad7dd6cbc
marso32 <marsolais32@gmail.com>
2016-01-21 20:03:06 -0500

    Update Gimbal.py

--- PROJECT/LIB/Gimbal.py
+++ PROJECT/LIB/Gimbal.py
@@ -3,21 +3,11 @@
 import sys
 import smbus
 import math
-
 import Acc
-from Servo import servo 
+from myclass import servo,pid,acc
+import time
 
-"""class accelerometre:
-	def init(ax,ay,az,gx,gy,gz,pitch,roll):
-		ax=0
-		ay=0
-		az=0
-		gx=0
-		gy=0
-		gz=0
-		pitch=1
-		roll=1
-"""
+
 DELTA_T = 0.05
 
 s1=servo(0x40,100,0,550)
